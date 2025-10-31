d531bd2d6a2e03a92a86ac75ec79ecc873bbd386
favoursamuel <favor.s@mblng.com>
2025-10-31 13:33:39 +0100

    update on ui

--- includes/head.php
+++ includes/head.php
@@ -16,6 +16,7 @@
     <?= $get->head_code ?>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
 </head>
 <body>
 <header id="header" class="header fixed-top" data-scrollto-offset="0"
